e6776cefd8e18cd176e2417a27b33738cb7bd1f1
robsoncorreia <robson.correia.bd@gmail.com>
2017-08-26 12:06:46 -0300

    Inicio do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreEP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
