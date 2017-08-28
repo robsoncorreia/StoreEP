e8e3bc0689813bb32e9cc5bfe55df2cd45624ca8
robsoncorreia <robson.correia.bd@gmail.com>
2017-08-28 19:23:56 -0300

    teste

--- StoreEP/Program.cs
+++ StoreEP/Program.cs
@@ -16,7 +16,6 @@ namespace StoreEP
         {
             BuildWebHost(args).Run();
         }
-
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
