caa2856c9c63b4e262d47de8d8171a9c0ab25bdd
Robson Correia <robsoncorreia01@outlook.com>
2017-09-20 09:20:10 -0300

    Login funcionando.

--- StoreEP/Controllers/AccountController.cs
+++ StoreEP/Controllers/AccountController.cs
@@ -60,30 +60,25 @@ namespace StoreEP.Controllers
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
+                ApplicationUser user = await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
                 {
-                    _logger.LogInformation("User logged in.");
-                    return RedirectToLocal(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToAction(nameof(LoginWith2fa), new { returnUrl, model.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToAction(nameof(Lockout));
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return View(model);
+                    await _signInManager.SignOutAsync();
+                    var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation($"Usuário {user.UserName} logado.");
+                        return RedirectToLocal(returnUrl);
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Falha na tentativa de login.");
+                        return View(model);
+                    }
+
                 }
             }
-            // If we got this far, something failed, redisplay form
             return View(model);
         }
 
