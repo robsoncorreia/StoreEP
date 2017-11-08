00ba34bee8c79773cb9627e99695343633d3daa1
Robson Correia <robsoncorreia01@outlook.com>
2017-11-08 15:39:10 -0200

    avaliações modificadas.

--- StoreEP/Models/Endereco.cs
+++ StoreEP/Models/Endereco.cs
@@ -11,23 +11,21 @@ namespace StoreEP.Models
         public int EnderecoID { get; set; }
         public string UserId { get; set; }
         [Required(ErrorMessage = "Entre com o endereço.")]
-        [Display(Name = "Rua")]
         public string Rua { get; set; }
         [Required(ErrorMessage = "Entre com o nome bairro.")]
         public string Bairro { get; set; }
+        [Display(Name = "Número")]
         [Required(ErrorMessage = "Entre com o número.")]
         [RegularExpression(@"^[0-9]{1,100}$", ErrorMessage = "Apenas números.")]
         public string Numero { get; set; }
         [Required(ErrorMessage = "Por favor digite o nome da cidade.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Apenas caractéres.")]
         public string Cidade { get; set; }
         [Required(ErrorMessage = "Por favor digite o nome do estado.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Apenas caractéres.")]
         public string Estado { get; set; }
         [Required(ErrorMessage = "Por favor digite o Cep.")]
         public string CEP { get; set; }
         [Required(ErrorMessage = "Digite o nome do pais.")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,100}$", ErrorMessage = "Apenas caractéres.")]
+        [Display(Name = "País")]
         public string Pais { get; set; }
         public string Complemento { get; set; }
         public DateTime DataUtilizacao { get; set; } = DateTime.Now;
