2cb0edcfbb20c6451bb741ac483c4425185d63b1
Robson Correia <robsoncorreia01@outlook.com>
2017-11-23 14:38:56 -0200

    layout do detalhe do produto ajustado.

---
+++ StoreEP/Data/Trigger/CartLine/dbo.Trigger.sql
@@ -0,0 +1,16 @@
+﻿CREATE TRIGGER [Trigger]
+	ON [dbo].[CartLine]
+	FOR INSERT
+	AS
+	BEGIN
+		DECLARE @id int, @estoque int, @quantidade int
+		SELECT @id = ProdutoID, @quantidade = Quantidade
+		FROM INSERTED
+
+	    SET @estoque = (SELECT Quantidade from Produto where ProdutoID = @id)
+		IF @estoque != 0 AND @quantidade <= @estoque
+
+		UPDATE Produto SET
+		Quantidade =  @estoque - @quantidade 
+		WHERE ProdutoID = @id
+	END
